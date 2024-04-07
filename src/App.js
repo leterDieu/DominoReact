4da70d15e2f6c25cc32ef486ff93009f5117cdf7
leterdieu <leterdieu@skiff.com>
2024-04-07 20:14:33 +0300

    hotfix2

--- src/App.js
+++ src/App.js
@@ -10,15 +10,7 @@ import {attack} from "./gameLogic/gamelogic.mjs";
 const deck = basic_deck
 
 
-const gameData = {turn: 0, stage: 0, playerMana: 5, botMana: 5, playerManaPerTurn: 2, botManaPerTurn: 2};
-// stages:
-// -1 - debug,
-// 0 - mana increase, cards drawing
-// 1 - player cards picking
-// 2 - bot cards picking
-// 3 - spells
-// 4 - player attacks
-// 5 - bot attacks
+// const GameData = {turn: 0, stage: 0, playerMana: 5, botMana: 5, playerManaPerTurn: 2, botManaPerTurn: 2};
 
 function App() {
     const [deckedCards] = useState(deck)
@@ -30,8 +22,6 @@ function App() {
     const [enemyTableCards, setEnemyTableCards] = useState([])
 
     const [tableChoice1, setTableChoice1] = useState(null)
-
-    const [stage, setStage] = useState(gameData.stage)
 
     const allData = {
         decks: deckedCards, setters: {
@@ -46,116 +36,72 @@ function App() {
 
 
     const handleHandClick = (card) => {
-        if (gameData.stage === 1 || gameData.stage === -1) {
-            let manage_HandCards = [...playerHandCards]
-            let oldTable = [...playerTableCards]
+        let manage_HandCards = [...playerHandCards]
+        let oldTable = [...playerTableCards]
 
-            oldTable.push(card)
-            manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
-            setPlayerHandCards(manage_HandCards)
-            setPlayerTableCards(oldTable)
-        }
+        oldTable.push(card)
+        manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
+        setPlayerHandCards(manage_HandCards)
+        setPlayerTableCards(oldTable)
     }
 
     const handleHandBot = (card) => {
-        if (gameData.stage === 2 || gameData.stage === -1) {
-            let manage_HandCards = [...enemyHandCards]
-            let oldTable = [...enemyTableCards]
+        let manage_HandCards = [...enemyHandCards]
+        let oldTable = [...enemyTableCards]
 
-            oldTable.push(card)
-            manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
-            setEnemyHandCards(manage_HandCards)
-            setEnemyTableCards(oldTable)
-        }
+        oldTable.push(card)
+        manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
+        setEnemyHandCards(manage_HandCards)
+        setEnemyTableCards(oldTable)
     }
 
     const handleTableClickOwn = (card) => {
-        if (gameData.stage === 4 || gameData.stage === -1) {
-            setTableChoice1(card)
-        }
+        setTableChoice1(card)
     }
 
     const handleTableClickOther = (card) => {
-        if (gameData.stage === 4 || gameData.stage === -1) {
-            if (tableChoice1 !== null) {
-                console.log(tableChoice1)
-                console.log(card)
+        if (tableChoice1 !== null) {
+            console.log(tableChoice1)
+            console.log(card)
 
-                attack(tableChoice1, card, allData.getters.player.table, allData.getters.enemy.table)
-            }
+            attack(tableChoice1, card, allData.getters.player.table, allData.getters.enemy.table)
         }
     }
-
-    const plusStage = () => {
-        if (gameData.stage === 0) {
-            // draw cards
-            gameData.stage++
-        }
-        else if (gameData.stage === 1) {
-            //bot cards picking
-            gameData.stage++
-        } else if (gameData.stage === 2) {
-            //spells
-            gameData.stage++
-        } else if (gameData.stage === 4) {
-            //bot attack
-            gameData.stage++
-        } else if (gameData.stage === 5) {
-            gameData.playerMana += gameData.playerManaPerTurn
-            gameData.botMana += gameData.botManaPerTurn
-            gameData.stage = 0
-            gameData.turn++
-        } else {
-            gameData.stage++
-        }
-        setStage(gameData.stage)
-    }
-
-    const debugStage = () => {
-        gameData.stage = -1
-        setStage(gameData.stage)
-    }
-    
 
     return (<div className="App">
-        <button onClick={plusStage}>stage++</button>
-        <button onClick={debugStage}>debug stage</button>
-        <p>{stage}</p>
+            <Header data={allData}/>
 
-        <Header data={allData} stage={gameData.stage}/>
+            <div className="allCards">
 
-        <div className="allCards">
+                <p>Enemy's hand:</p>
 
-            <p>Enemy's hand:</p>
+                <div className="enemy-hand">
+                    {enemyHandCards.map(card => (<UltimateCard key={card.id} card={card} func={handleHandBot}/>))}
+                </div>
 
-            <div className="enemy-hand">
-                {enemyHandCards.map(card => (
-                    <UltimateCard key={card.id} card={card} func={handleHandBot}/>))}
+                <p>Enemy's table:</p>
+
+                <div className="enemy-table">
+                    {enemyTableCards.map(card => (
+                        <UltimateCard key={card.id} card={card} func={handleTableClickOther}/>))}
+                </div>
+
+                <p>Player's table:</p>
+
+                <div className="player-table">
+                    {playerTableCards.map(card => (
+                        <UltimateCard key={card.id} card={card} func={handleTableClickOwn}/>))}
+                </div>
+
+                <p>Player's hand:</p>
+
+                <div className="player-hand">
+                    {playerHandCards.map(card => (<UltimateCard key={card.id} card={card} func={handleHandClick}/>))}
+                </div>
+
             </div>
 
-            <p>Enemy's table:</p>
-
-            <div className="enemy-table">
-                {enemyTableCards.map(card => (
-                    <UltimateCard key={card.id} card={card} func={handleTableClickOther}/>))}
-            </div>
-
-            <p>Player's table:</p>
-
-            <div className="player-table">
-                {playerTableCards.map(card => (
-                    <UltimateCard key={card.id} card={card} func={handleTableClickOwn}/>))}
-            </div>
-
-            <p>Player's hand:</p>
-
-            <div className="player-hand">
-                {playerHandCards.map(card => (<UltimateCard key={card.id} card={card} func={handleHandClick}/>))}
-            </div>
-
-        </div>
-
-    </div>);
+        </div>);
 }
 
 export default App;
