eaa907b22afce0858f0643c6f50114be1eccf5fc
M4ntekot <mantekofe@gmail.com>
2024-04-11 21:48:22 +0300

    added classnames to recognize blocks in browsers

--- src/App.js
+++ src/App.js
@@ -11,17 +11,15 @@ import {Boris} from "./gameLogic/card.mjs";
 const deck = basic_deck
 
 
-const gameData = {
-    turn: 0,
-    stage: 0,
-    playerMana: 12321321,
-    botMana: 213123231,
-    playerManaPerTurn: 2,
-    botManaPerTurn: 2,
-    hasTakenCard: false,
-    playerHp: 10,
-    botHp: 10
-};
+const gameData = {turn: 0, stage: 0, playerMana: 12321321, botMana: 213123231, playerManaPerTurn: 2, botManaPerTurn: 2, hasTakenCard: false};
+// stages:
+// -1 - debug,
+// 0 - mana increase, cards drawing
+// 1 - player cards picking
+// 2 - bot cards picking
+// 3 - spells
+// 4 - player attacks
+// 5 - bot attacks
 
 function App() {
     const [deckedCards] = useState(deck)
@@ -50,26 +48,32 @@ function App() {
 
 
     const handleHandClick = (card) => {
-        if (gameData.stage === 1) {
-            if (gameData.playerMana < card.cst) {
-                alert("You have not enough mana to get this card on table")
-                return
-            }
-            let manage_HandCards = [...playerHandCards]
-            let oldTable = [...playerTableCards]
-
-            oldTable.push(card)
-            manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
-            setPlayerHandCards(manage_HandCards)
-            setPlayerTableCards(oldTable)
-            gameData.playerMana -= card.cst
+        if (gameData.playerMana < card.cst) {
+            alert("You have not enough mana to get this card on table")
+            return
         }
+        let manage_HandCards = [...playerHandCards]
+        let oldTable = [...playerTableCards]
+
+        oldTable.push(card)
+        manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
+        setPlayerHandCards(manage_HandCards)
+        setPlayerTableCards(oldTable)
+        gameData.playerMana -= card.cst
+    }
+
+    const handleHandBot = (card) => {
+        let manage_HandCards = [...enemyHandCards]
+        let oldTable = [...enemyTableCards]
+
+        oldTable.push(card)
+        manage_HandCards.splice(manage_HandCards.indexOf(card), 1)
+        setEnemyHandCards(manage_HandCards)
+        setEnemyTableCards(oldTable)
     }
 
     const handleTableClickOwn = (card) => {
-        if (gameData.stage === 4) {
-            setTableChoice1(card)
-        }
+        setTableChoice1(card)
     }
 
     const handleTableClickOther = (card) => {
@@ -79,11 +83,6 @@ function App() {
 
             attack(tableChoice1, card, allData.getters.player.table, allData.getters.enemy.table, allData.setters.enemy.table)
         }
-    }
-
-    const botAttack = () => {
-        let rng_card = enemyTableCards[randint(enemyTableCards.length)]
-        attack(tableChoice1, rng_card, allData.getters.enemy.table, allData.getters.player.table, allData.setters.player.table)
     }
 
     const Mana = (props) => {
@@ -99,7 +98,7 @@ function App() {
         const container = props.container
 
         return (
-            <div>
+            <div className="cardContainer">
                 <p>{name}:</p>
                 <div className={"stdBlock"}>
                     {container.map(card => (
@@ -109,133 +108,32 @@ function App() {
         )
     }
 
-    const drawCards = (handGetter, handSetter) => { // ограничение на ману и количество карт
-        let copied_hand = [...handGetter]
+    return (
+        <div className="mainWindow">
+            <div className="headerWindow">
+                <Header data={allData} stage={stage} setStage={setStage} turn={turnConst} setTurn={setTurnConst} hasTakenCard={gameData.hasTakenCard}/>
 
-        if (copied_hand.length === 8) {
-            return
-        }
+                <Mana mana={gameData.playerMana}/>
+            </div>
+            <div className="bodyWindow">
+                <div className="enemyHand">
+                    <Field name={"Enemy's hand"} func={handleHandBot} container={enemyHandCards}/>
+                </div>
 
-        let newCard = new deck[randint(deck.length)]()
-        copied_hand.push(newCard)
-        handSetter(copied_hand)
-    }
+                <div className="enemyTable">
+                    <Field name={"Enemy's table"} func={handleTableClickOther} container={enemyTableCards}/>
+                </div>
 
-    const botPick = () => {
-        let manage_HandCards = [...enemyHandCards]
-        let manage_TableCards = [...enemyTableCards]
+                <div className="playerTable">
+                    <Field name={"Player's table"} func={handleTableClickOwn} container={playerTableCards}/>
+                </div>
 
-        let card_index = randint(manage_HandCards.length)
-
-        manage_TableCards.push(manage_HandCards[card_index])
-        manage_HandCards.splice(card_index)
-        setEnemyHandCards(manage_HandCards)
-        setEnemyTableCards(manage_TableCards)
-    }
-
-    const manaIncrease = () => {
-        gameData.playerMana += gameData.playerManaPerTurn
-        gameData.botMana += gameData.botManaPerTurn
-    }
-
-    const applySpells = () => {
-        //
-    }
-
-    // stages:
-    // -1 - debug,
-    // 0 - mana increase, cards drawing - stdMana++ ; drawCards => sys
-    // 1 - player cards picking => ready button
-    // 2 - bot cards picking => sys
-    // 3 - spells => sys
-    // 4 - player attacks => ready button
-    // 5 - bot attacks => sys
-
-
-    const stagePlus = () => {
-        // this block is separate of easier readability
-        if (gameData.stage === 5) {
-            gameData.stage = 0
-        } else {
-            gameData.stage++
-        }
-
-        setStage(gameData.stage)
-
-        if (gameData.stage === 0) {
-            manaIncrease()
-            drawCards(playerHandCards, setPlayerHandCards)
-            drawCards(enemyHandCards, setEnemyHandCards)
-            stagePlus()
-        } else if (gameData.stage === 1) {
-            // implemented
-            // attention! no stagePlus required here, as it's written in the ready button
-        } else if (gameData.stage === 2) {
-            botPick()
-            stagePlus()
-        } else if (gameData.stage === 3) {
-            applySpells()
-            checkWin()
-            stagePlus()
-        } else if (gameData.stage === 4) {
-            // implemented
-            // attention! no stagePlus required here, as it's written in the ready button
-        } else if (gameData.stage === 5) {
-            checkWin()
-            botAttack()
-            checkWin()
-            stagePlus()
-        }
-    }
-
-    const checkWin = () => {
-        if (gameData.playerHp <= 0) {
-            alert('You lost, loser!')
-        } else if (gameData.botHp <= 0) {
-            alert('You won, gg.')
-        }
-    }
-
-    const blankFunc = () => {
-        //
-    }
-
-    const readyButton = () => {
-        if (gameData.stage === 1 || gameData.stage === 4) {
-            stagePlus()
-        }
-    }
-
-    return (<div>
-
-        <div>
-            <Header data={allData} stage={stage} setStage={setStage} turn={turnConst} setTurn={setTurnConst}
-                    hasTakenCard={gameData.hasTakenCard}/>
-
-            <Mana mana={gameData.playerMana}/>
-        </div>
-
-        <button onClick={readyButton}>Ready</button>
-
-        <div>
-            <div>
-                <Field name={"Enemy's hand"} func={blankFunc} container={enemyHandCards}/>
+                <div className="playerHand">
+                    <Field name={"Player's hand"} func={handleHandClick} container={playerHandCards}/>
+                </div>
             </div>
 
-            <div>
-                <Field name={"Enemy's table"} func={handleTableClickOther} container={enemyTableCards}/>
-            </div>
-
-            <div>
-                <Field name={"Player's table"} func={handleTableClickOwn} container={playerTableCards}/>
-            </div>
-
-            <div>
-                <Field name={"Player's hand"} func={handleHandClick} container={playerHandCards}/>
-            </div>
-        </div>
-
-    </div>);
+        </div>);
 }
 
 export default App;
